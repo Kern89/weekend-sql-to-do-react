fee50062d94ca49890c11d3d3469d88d820d0699
Kenneth Grina <grina.kenneth@gmail.com>
2024-03-21 17:17:59 -0500

    database.sql made and creat table query added

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
-
+router.get('/', (req, res) => {
+    const queryText = `SELECT * FROM "todo"`
+})
 // POST
 
 // PUT
