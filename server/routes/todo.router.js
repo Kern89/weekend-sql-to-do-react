2be3993fb788dd5167335b6d8c88167a1516c580
Kenneth Grina <grina.kenneth@gmail.com>
2024-03-21 17:29:39 -0500

    GET made in router

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -4,8 +4,14 @@ const pool = require('../modules/pool.js');
 
 // GET
 router.get('/', (req, res) => {
-    const queryText = `SELECT * FROM "todo"`
-})
+    const queryText = `SELECT * FROM "todo";`;
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('Error in GET /todo', error);
+        res.sendStatus(500);
+    })
+});
 // POST
 
 // PUT
