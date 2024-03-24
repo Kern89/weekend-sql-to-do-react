6b00bf67f2ebfcfd2a8f0e44e31170a4115bac6e
Kenneth Grina <grina.kenneth@gmail.com>
2024-03-23 20:53:12 -0500

    form made in app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,10 +4,12 @@ import axios from 'axios';
 
 function App () {
   
+  const [duedate, setDueDate] = useState('');
+  const [todoItem, setTodoItem] = useState('');
   const [todoList, setTodoList] = useState([]);
 
   const getTodoList = () => {
-    Axios.get('/api/todo').then((response) => {
+    axios.get('/api/todo').then((response) => {
       console.log('Data:', response.data);
       setTodoList(response.data);
     }).catch((error) => {
@@ -16,11 +18,20 @@ function App () {
     })
   };
 
-
+useEffect(() => {
+  getTodoList();
+}, []);
 
   return (
     <div>
       <h1>TO DO APP</h1>
+      <form>
+        New task: <input type="text" value={todoItem} 
+        onChange={(e) => setTodoItem(e.target.value)} />
+        Due on: <input type="date" value={duedate} 
+        onChange={(e) => setDueDate(e.target.value)} />
+        <input type="submit" value="submit" />
+      </form>
     </div>
   );
 
